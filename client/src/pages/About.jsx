984dec8b8e78c7c7fd208555cf50e7e904ee5fae
DAObliterator <savyasajsaradh@gmail.com>
2024-04-06 21:20:05 +0530

    api call added in about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React , { useState , useEffect} from 'react'
 
 export const About = () => {
+
+  useEffect(() => {
+
+     axios
+       .post(
+         `${import.meta.env.VITE_API}/auth/dummy-route`,
+         { withCredentials: true }
+       )
+       .then((response) => {
+         if (response.status === 200) {
+          alert("user is logged in");
+         }
+       })
+       .catch((error) => {
+        console.log( error , "error happened at /auth/dummy-route");
+       });
+
+  },[])
+
+
   return (
     <div>About</div>
   )
